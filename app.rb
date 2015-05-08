f96af98d0e03e32e84a68c63e97212aeb3bc7720
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-08 16:32:35 -0700

    Add routing for client and clients.

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require('sinatra')
 require('sinatra/reloader')
 require('pry')
-require('pg')
 require('./lib/client')
 require('./lib/stylist')
-also_reload('/lib/**/*.rb')
+also_reload('lib/**/*.rb')
 
 if defined?(DB) == nil
+  require('pg')
   DB = PG.connect(dbname: 'hair_salon')
 end
 
@@ -21,6 +21,50 @@ get('/reset') do
 end
 
 get('/stylists') do
-  @stylists = Stylists.all
+  @stylists = Stylist.all
   erb(:stylists)
+end
+
+get('/stylists/add') do
+  erb(:stylist_form)
+end
+
+post('/stylists/add') do
+  first_name = params['first_name']
+  last_name = params['last_name']
+  stylist = Stylist.new(id: nil, first_name: first_name, last_name: last_name)
+  stylist.save
+  redirect to("/stylists/#{stylist.id}")
+end
+
+get('/stylists/:id') do
+  @stylist = Stylist.find(id: params['id'].to_i).first
+  erb(:stylist)
+end
+
+get('/clients') do
+  @clients = Client.all
+  erb(:clients)
+end
+
+get('/clients/add') do
+  @stylists = Stylist.all
+  erb(:client_form)
+end
+
+post('/clients/add') do
+  first_name = params['first_name']
+  last_name = params['last_name']
+  stylist = params['stylist']
+  if stylist == ""
+    stylist = nil
+  end
+  client = Client.new(id: nil, first_name: first_name, last_name: last_name, stylist_id: stylist)
+  client.save
+  redirect to("/clients/#{client.id}")
+end
+
+get('/clients/:id') do
+  @client = Client.find(id: params['id'].to_i).first
+  erb(:client)
 end
