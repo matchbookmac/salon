bba07479d8d2a83c618a47e4973d698af6e438f0
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-08 10:15:46 -0700

    Add .all method to Client.

--- lib/client.rb
+++ lib/client.rb
@@ -6,6 +6,23 @@ class Client
     @first_name = attributes[:first_name]
     @last_name = attributes[:last_name]
     @stylist_id = attributes[:stylist_id]
-
   end
+
+  def self.all
+    clients = []
+    returned_clients = DB.exec("SELECT * FROM clients;")
+    returned_clients.each do |client|
+      id = client['id'].to_i
+      first_name = client['first_name']
+      last_name = client['last_name']
+      stylist_id = client['stylist_id'].to_i
+      clients << Client.new(id: id, first_name: first_name, last_name: last_name, stylist_id: stylist_id)
+    end
+    clients
+  end
+
+  # def save
+  #   result = DB.exec("INSERT INTO clients (first_name, last_name, stylist_id) VALUES ('#{@first_name}', '#{@last_name}', #{@stylist_id}) RETURNING id;")
+  #   @id = result.first['id'].to_i
+  # end
 end
--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -1,9 +1,6 @@
 require('spec_helper')
 
 describe(Client) do
-  # describe('#id') do
-  #
-  # end
 
   describe('#first_name') do
     it('will return the first name of a client') do
@@ -19,20 +16,25 @@ describe(Client) do
     end
   end
 
-  # # describe('#stylist_id') do
-  #
-  # end
+  describe('.all') do
+    it('will return an empty array when there are no clients') do
+      expect(Client.all).to(eq([]))
+    end
+  end
 
-  # describe('.all') do
-  #   it('will return an empty array when there are no clients') do
-  #     expect(Client.all).to(eq([]))
-  #   end
-  # end
   # describe('#save') do
   #   it('will return an array of clients when there are clients') do
   #     client = Client.new(id: nil, first_name: 'Ian', last_name: 'MacDonald')
   #     client.save
   #     expect(Client.all).to(eq([]))
   #   end
+  # end
+
+  # describe('#id') do
+  #
+  # end
+
+  # # describe('#stylist_id') do
+  #
   # end
 end
