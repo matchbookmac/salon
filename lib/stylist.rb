e5e8f4b3ca7bf2db20a2a990d4a43853e3a22258
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-08 11:47:30 -0700

    Add initialize and all method to Stylist class.

--- lib/stylist.rb
+++ lib/stylist.rb
@@ -1,3 +1,23 @@
 class Stylist
+  attr_reader(:id, :first_name, :last_name)
+
+  def initialize(attributes)
+    @id = attributes[:id]
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+  end
+
+  def self.all
+    stylists = []
+    returned_stylists = DB.exec("SELECT * FROM stylists;")
+    returned_stylists.each do |stylist|
+      id = stylist['id'].to_i
+      first_name = stylist['first_name']
+      last_name = stylist['last_name']
+      stylist_id = stylist['stylist_id'].to_i
+      stylists << Stylist.new(id: id, first_name: first_name, last_name: last_name)
+    end
+    stylists
+  end
 
 end
--- spec/stylist_spec.rb
+++ spec/stylist_spec.rb
@@ -1,1 +1,24 @@
 require('spec_helper')
+
+describe(Stylist) do
+
+  describe('#first_name') do
+    it('will return the first name of a stylist') do
+      stylist = Stylist.new(id: nil, first_name: 'Joe', last_name: 'The Barber')
+      expect(stylist.first_name).to(eq('Joe'))
+    end
+  end
+
+  describe('#last_name') do
+    it('will return the last name of a stylist') do
+      stylist = Stylist.new(id: nil, first_name: 'Joe', last_name: 'The Barber')
+      expect(stylist.last_name).to(eq('The Barber'))
+    end
+  end
+
+  describe('.all') do
+    it('will return an empty array when there are no stylists') do
+      expect(Stylist.all).to(eq([]))
+    end
+  end
+end
