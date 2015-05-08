5736b8c8fd43f3b3c5e634061ec46a1b440c691f
Ian on MacBook Pro <imacd20@gmail.com>
2015-05-08 15:03:36 -0700

    Stylists page will list stylists available.

--- spec/hair_salon_integration_test.rb
+++ spec/hair_salon_integration_test.rb
@@ -11,13 +11,24 @@ describe('the hair salon path') do
   end
 
   describe('stylists') do
-    it('will navigate to stylists and show a list of available stylists and navigate home') do
+    it('will navigate to stylists and show no stylists if there are none and navigate home') do
       visit('/stylists')
       expect(page).to have_content('Sorry, there are no stylists at this time.')
       click_on('Home')
       expect(page).to have_content('Hair by Ian!')
       expect(page).to have_content('Stylists')
       expect(page).to have_content('Clients')
+    end
+
+    it('will navigate to stylists and show a list of stylists') do
+      stylist_1 = Stylist.new(id: nil, first_name: 'Joe', last_name: 'The Barber')
+      stylist_1.save
+      stylist_2 = Stylist.new(id: nil, first_name: 'Bill', last_name: 'The Hairdresser')
+      stylist_2.save
+      visit('/stylists')
+      expect(page).to have_content('Here is a list of available stylists:')
+      expect(page).to have_content('Joe The Barber')
+      expect(page).to have_content('Bill The Hairdresser')
     end
   end
 end
